d908a324c6b5b735f554b1aa623079c170a008ad
Francisco Lopes <85237841+FranciscoLopes21@users.noreply.github.com>
2023-12-28 18:51:28 +0000

    Registar Client

--- BDII_Web/BDII_App/views.py
+++ BDII_Web/BDII_App/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from django.db import connection
 from .forms import formularioRegisto, formualarioRegistoEquipamentos
 from django.contrib import messages
 from django.contrib.auth.hashers import make_password, check_password
+from django.contrib.auth import login, authenticate
 
 
 # Create your views here.
@@ -17,7 +18,34 @@ def room(request):
 
 # regista um novo utilizador
 def registarUtilizador(request):
-    form = formularioRegisto()
+    if request.method == 'POST':
+        form = formularioRegisto(request.POST)
+        if form.is_valid():
+            nome = form.cleaned_data["nome"]
+            apelido = form.cleaned_data["apelido"]
+            data_nascimento = form.cleaned_data["data_nascimento"]
+            morada = form.cleaned_data["morada"]
+            email = form.cleaned_data["email"]
+            password = make_password(form.cleaned_data["password"])
+            tipo_user = "Client"
+
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    "INSERT INTO users (nome, apelido, data_nascimento, morada, email, password, tipo_user) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                    [nome, apelido, data_nascimento, morada, email, password, tipo_user]
+                )
+
+                # Autenticar o usuário recém-criado
+                user = authenticate(request, username=email, password=password)
+                if user is not None:
+                    login(request, user)
+
+                # Redirecionar para a página inicial ou outra página desejada
+                return redirect('room')
+
+    else:
+        form = formularioRegisto()
+
     return render(request, 'Registar.html', {'form': form})   
 
 # regista um novo utilizador
--- BDII_Web/BDII_Web/settings.py
+++ BDII_Web/BDII_Web/settings.py
@@ -79,9 +79,13 @@ WSGI_APPLICATION = 'BDII_Web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'BDII-Grupo11',
+        'USER': 'postgres',
+        'PASSWORD': 'Fibaril21!',
+        'HOST': 'localhost', 
+        'PORT': '5432',
+    },
 }
 
 
